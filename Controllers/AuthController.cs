a7b5ce90d0d83ae51ee9e5b2e7c580a8f04fdf96
Ujjwal Chalise <Ujjwalchalise9@gmail.com>
2024-08-21 11:03:46 +0545

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Facebook;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+public class AuthController : Controller
+{
+    [HttpGet("login-facebook")]
+    public IActionResult LoginFacebook()
+    {
+        var redirectUrl = Url.Action("FacebookResponse", "Auth");
+        var properties = new AuthenticationProperties { RedirectUri = redirectUrl };
+        return Challenge(properties, FacebookDefaults.AuthenticationScheme);
+    }
+
+    [HttpGet("facebook-response")]
+    public async Task<IActionResult> FacebookResponse()
+    {
+        var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        if (result?.Principal == null)
+            return RedirectToAction(nameof(LoginFacebook));
+
+        var claims = result.Principal.Identities
+                        .FirstOrDefault()?.Claims.Select(claim => new
+                        {
+                            claim.Type,
+                            claim.Value
+                        });
+
+        return Json(claims);
+    }
+
+    [Authorize]
+    public IActionResult Profile()
+    {
+        return View(User.Claims.Select(claim => new { claim.Type, claim.Value }));
+    }
+
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        return RedirectToAction("Index", "Home");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Facebook;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Add Authentication services
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = FacebookDefaults.AuthenticationScheme;
+})
+.AddCookie()
+
+.AddFacebook(options =>
+{
+    options.AppId = "1684281592350201";
+    options.AppSecret = "51ad0b358a4a8acd098a13a2c3aa035e";
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication(); // Add this line to enable authentication
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
